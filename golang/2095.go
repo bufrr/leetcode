0be393446117b5c4e634a498793121c950229a29
bufrr <chouyc.adam@gmail.com>
2024-02-25 16:11:54 +0800

    2095

---
+++ golang/2095.go
@@ -0,0 +1,14 @@
+func deleteMiddle(head *ListNode) *ListNode {
+    if head.Next == nil {
+        return nil
+    }
+    slow := head
+    fast := head.Next.Next
+
+    for fast != nil && fast.Next != nil {
+        fast = fast.Next.Next
+        slow = slow.Next
+    }
+    slow.Next = slow.Next.Next
+    return head
+}

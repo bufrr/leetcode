4f389066d7d59d989ad252b3c3f3b50acbec97e9
bufrr <chouyc.adam@gmail.com>
2025-03-30 17:13:06 +0800

    0872

--- rust/0872.rs
+++ rust/0872.rs
@@ -1,22 +1,43 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
 use std::rc::Rc;
 use std::cell::RefCell;
 impl Solution {
     pub fn leaf_similar(root1: Option<Rc<RefCell<TreeNode>>>, root2: Option<Rc<RefCell<TreeNode>>>) -> bool {
-        let mut leaf1 = vec![];
-        let mut leaf2 = vec![];
-        dfs(&root1, &mut leaf1);
-        dfs(&root2, &mut leaf2);
-        leaf1 == leaf2
-    }
-}
-
-pub fn dfs(root: &Option<Rc<RefCell<TreeNode>>>, leaf: &mut Vec<i32>) {
-    if let Some(node) = root {
-        let node = node.borrow();
-        if node.left.is_none() && node.right.is_none() {
-            leaf.push(node.val);
+        fn dfs(root: Option<Rc<RefCell<TreeNode>>>, leaves: &mut Vec<i32>) {
+            if root.is_none() {
+                return 
+            }
+            let node = root.as_ref().unwrap().borrow();
+            if node.left.is_none() && node.right.is_none() {
+                leaves.push(node.val);
+            }
+            dfs(node.left.clone(), leaves);
+            dfs(node.right.clone(), leaves);
         }
-        dfs(&node.left, leaf);
-        dfs(&node.right, leaf);
+
+        let mut vec1: Vec<i32> = Vec::new();
+        let mut vec2: Vec<i32> = Vec::new();
+
+        dfs(root1, &mut vec1);
+        dfs(root2, &mut vec2);
+
+        vec1 == vec2
     }
 }

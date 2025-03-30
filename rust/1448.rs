4a8830a2b6055ae4e8a388bcdcc1209f782c0505
bufrr <chouyc.adam@gmail.com>
2025-03-30 19:03:33 +0800

    1448

--- rust/1448.rs
+++ rust/1448.rs
@@ -1,17 +1,43 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
 use std::rc::Rc;
 use std::cell::RefCell;
 impl Solution {
     pub fn good_nodes(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        Self::dfs(&root, i32::MIN)
-    }
-
-    pub fn dfs(node: &Option<Rc<RefCell<TreeNode>>>, max: i32) -> i32{
-        if let Some(node) = node {
-            let node = node.borrow();
-            let max_till_now = std::cmp::max(node.val, max);
-            Self::dfs(&node.left, max_till_now) + Self::dfs(&node.right, max_till_now) + (node.val >= max) as i32
-        } else {
-            0
+        fn dfs(root: Option<Rc<RefCell<TreeNode>>>, max_so_far: i32, good: &mut i32) {
+            if root.is_none() {
+                return
+            }
+            let node = root.as_ref().unwrap().borrow();
+            if node.val >= max_so_far {
+                *good += 1;
+            } 
+            let new_max = max_so_far.max(node.val);
+            dfs(node.left.clone(), new_max, good);
+            dfs(node.right.clone(), new_max, good);
         }
+        if root.is_none() {
+            return 0;
+        }
+        let mut root_val = root.as_ref().unwrap().borrow().val;
+        let mut good = 0;
+        dfs(root, root_val, &mut good);
+        good
     }
 }
